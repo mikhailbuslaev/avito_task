6285c252be5a96f9c5c8f52d260179c270eb1288
Михаил Буслаев <buslaevnmh@yandex.ru>
2022-02-08 20:19:33 +0300

    added transaction http-operator in server/server.go

--- server/server.go
+++ server/server.go
@@ -5,9 +5,15 @@ import (
 	"log"
 	"net/http"
 	"time"
-
+	"encoding/json"
 	"github.com/gorilla/mux"
 )
+
+type TransactionTask struct {
+
+	SenderId	string
+	RecieverId 	string
+}
 
 func HomeHandler(w http.ResponseWriter, req *http.Request) {
 
@@ -16,15 +22,29 @@ func HomeHandler(w http.ResponseWriter, req *http.Request) {
 
 func TransactionHandler(w http.ResponseWriter, req *http.Request) {
 
-	fmt.Fprintf(w, "hello\n")
+		transaction := TransactionTask{}
+
+		err := req.ParseForm()
+	
+		if err != nil {
+			fmt.Println(fmt.Errorf("Error: %v", err))
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	
+		err = json.NewDecoder(req.Body).Decode(&transaction)
+		if err != nil {
+			http.Error(w, err.Error(), 400)
+			return
+		}
+		fmt.Println(transaction.SenderId)
 }
 
 func main() {
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", HomeHandler).Methods("GET")
-	router.HandleFunc("/transactions", HomeHandler).Methods("GET", "POST")
-	http.Handle("/", router)
+	router.HandleFunc("/transactions", TransactionHandler).Methods("GET", "POST")
 
 	server := &http.Server{
 		Handler:      router,
@@ -32,6 +52,8 @@ func main() {
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
+
+	fmt.Println("Server run...")
 
 	log.Fatal(server.ListenAndServe())
 }
