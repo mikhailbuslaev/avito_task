fa43b0a31cd7f983fb0531c0fe3ffb1b7379d999
Михаил Буслаев <buslaevnmh@yandex.ru>
2022-02-13 18:17:32 +0300

    added getbalance method

--- server.go
+++ server.go
@@ -1,13 +1,12 @@
 package main
 
 import (
+	"avitotask/db"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"time"
-
-	"avitotask/db"
 
 	"github.com/gorilla/mux"
 )
@@ -17,16 +16,59 @@ func HomeHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "hello\n")
 }
 
+func GetBalanceHandler(w http.ResponseWriter, req *http.Request) {
+
+	database := db.Connect()
+
+	wallet := ReadHttpGetBalanceRequest(w, req)
+
+	wallet.Balance = wallet.GetBalance(database)
+
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+
+	resp := make(map[string]float32)
+	resp["Balance"] = wallet.Balance
+
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+	}
+
+	w.Write(jsonResp)
+}
+
+func ReadHttpGetBalanceRequest(w http.ResponseWriter, req *http.Request) db.Wallet {
+
+	var wallet db.Wallet
+	err := req.ParseForm()
+
+	if err != nil {
+		log.Fatal(err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	err = json.NewDecoder(req.Body).Decode(&wallet)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return wallet
+}
+
 func TransactionHandler(w http.ResponseWriter, req *http.Request) {
 
 	database := db.Connect()
 	t := db.TransactionTask{}
-	t = ReadHttpRequest(w , req)
+	t = ReadHttpTransactionRequest(w, req)
 
 	t.TransactionCheck(database)
+
+	if t.Status == "approved" {
+		t.MakeTransaction(database)
+	}
 }
 
-func ReadHttpRequest(w http.ResponseWriter, req *http.Request) db.TransactionTask{
+func ReadHttpTransactionRequest(w http.ResponseWriter, req *http.Request) db.TransactionTask {
 
 	t := db.TransactionTask{}
 	err := req.ParseForm()
@@ -47,7 +89,8 @@ func main() {
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", HomeHandler).Methods("GET")
-	router.HandleFunc("/transactions", TransactionHandler).Methods("GET", "POST")
+	router.HandleFunc("/transactions", TransactionHandler).Methods("POST")
+	router.HandleFunc("/getbalance", GetBalanceHandler).Methods("POST")
 
 	server := &http.Server{
 		Handler:      router,
